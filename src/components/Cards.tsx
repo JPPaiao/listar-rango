16b4f1316e36754f23885275e38ebc800ca686a6
jplp <jp.paiao03@gmail.com>
2023-11-09 19:06:20 -0300

    style: Adicionado componente cards de listar restaurantes

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,20 @@
+import restaurantLogo from "../assets/vegan-restaurant-logo-design_1438-10.png"
+
 const Cards: React.FC = () => {
   return (
     <div>
-      cards
+      <div className="font-sans flex gap-6 justify-between items-center shadow-md rounded px-3 py-5 w-full">
+        <div className="w-24">
+          <img src={restaurantLogo} alt="Restaurant logo" />
+        </div>
+        <div className="text-zinc-600 w-full">
+          <h2 className="font-semibold text-base">Nome do restaurante</h2>
+          <h3 className="text-xs">Endereço do restaurante</h3>
+        </div>
+      </div>
+      <div className="relative w-12 h-auto bg-blue-900 bottom-24 left-72 p-2 text-[.50rem] text-center rounded-full text-white">
+        Aberto agora
+      </div>
     </div>
   )
 }
