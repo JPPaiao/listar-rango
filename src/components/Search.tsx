1b4d02bece1d08ac0d03e5162aa90f5236a3049e
jplp <jp.paiao03@gmail.com>
2023-11-09 19:05:20 -0300

    style: Adicionado o estilo do component Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -8,7 +8,7 @@ const Search: React.FC<Props> = ({ placeholder }) => {
   return (
     <form action="#" className="flex w-[600px]">
       <input type="text" placeholder={placeholder}
-        className="outline-none px-7 py-2 rounded-full shadow-md w-full bg-gray-50"
+        className="outline-none px-7 py-2 bg-[#fffefe] rounded-full shadow-md w-full"
       />
       <img src={searchIcon} alt="search" 
         className="w-4 relative right-8 cursor-pointer"
--- src/routers/ListRestaurants.tsx
+++ src/routers/ListRestaurants.tsx
@@ -1,4 +1,5 @@
 import { Search } from "../components/Search"
+import { Cards } from "../components/Cards"
 
 const ListRestaurants: React.FC = () => {
   return (
@@ -6,13 +7,15 @@ const ListRestaurants: React.FC = () => {
       <header className="flex gap-5 flex-col items-center">
         <div className="w-full bg-cyan-600 h-10">
         </div>
-        <h1 className="text-2xl ">
+        <h1 className="text-2xl text-zinc-600">
           Bem-vindo ao Lista Rango
         </h1>
         <Search placeholder={"Buscar estabelecimento"} />
       </header>
-      <main>
-
+      <main className="px-2 py-12 flex justify-evenly items-center flex-wrap gap-2">
+        <Cards />
+        <Cards />
+        <Cards />
       </main>
     </div>
   )
