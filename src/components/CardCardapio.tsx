1ac8d0b86b795b83dfc84326e2cf02c411204e72
jplp <jp.paiao03@gmail.com>
2023-11-11 17:23:50 -0300

    style: Layout da pagina de restaurante finalizado

--- src/components/CardCardapio.tsx
+++ src/components/CardCardapio.tsx
@@ -2,15 +2,16 @@ import pratoRestaurant from "../assets/prato-de-restaurante-vegetariano.png"
 
 const CardCardapio: React.FC = () => {
   return (
-    <div className="flex gap-3 max-w-xs shadow-box w-full">
+    <div className="flex gap-3 max-w-sm shadow-box  h-[116px] rounded-r-md">
       <img src={pratoRestaurant} alt="" 
-        className="w-24"
+        className="w-28"
       />
-      <div >
-        <h2>Nome do prato</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, harum vero.</p>
-        <div>
-          <span>R$</span>
+      <div className="px-1 py-1 flex flex-col justify-center gap-2 font-mont w-full">
+        <h2 className="text-lg font-semibold">Nome do prato</h2>
+        <p className="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <div className="flex gap-2 items-end">
+          <span className="text-cyan-500 font-semibold text-base">R$ 19,00</span>
+          <span className="text-xs line-through">R$ 19,00</span>
         </div>
       </div>
     </div>
--- src/routers/Restaurant.tsx
+++ src/routers/Restaurant.tsx
@@ -1,41 +1,73 @@
-import restaurantLogo from "../assets/vegan-restaurant-logo-design_1438-10.png"
+import restaurantLogo from "../assets/vegan-restaurant-logo-design_1438-10@2x.png"
+import chevronDown from "../assets/chevron-down.svg"
 import { CardCardapio } from "../components/CardCardapio"
 import { Search } from "../components/Search"
-
 
 const Restaurant: React.FC = () => {
   return (
     <div>
-      <div className="w-full bg-cyan-600 h-14">
+      <div className="w-full bg-cyan-600 h-14 shadow-box">
       </div>
-      <header className="max-w-4xl m-auto py-6">
-        <div className="flex items-center">
-          <img src={restaurantLogo} alt="Logo do restaurant" 
-            className="w-36"
-          />
-          <div className="max-w-md text-zinc-700">
-            <h1 className="text-3xl font-bold">Nome do restaurante</h1>
-            <p className="text-sm py-2">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit dignissimos excepturi aperiam laudantium, voluptates tempora sunt nam omnis delectus deleniti 
-            </p>
-            <div className="text-xs flex flex-col">
-              <span>Segunda à Sexta: 11:30 às 15:00</span>
-              <span>Sabados: 11:30 às 22:00</span>
-              <span>Domingos e feriados: 11:30 às 15:00</span>
+      <div className="max-w-6xl m-auto">      
+        <header className="max-w-4xl pt-6">
+          <div className="flex items-center">
+            <img src={restaurantLogo} alt="Logo do restaurant" 
+              className="w-36"
+            />
+            <div className="max-w-md text-zinc-700 font-mont">
+              <h1 className="text-3xl font-medium">Nome do restaurante</h1>
+              <p className="text-sm py-2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit dignissimos excepturi aperiam laudantium, voluptates tempora sunt nam omnis delectus deleniti
+              </p>
+              <div className="text-xs flex flex-col">
+                <span>Segunda à Sexta: 11:30 às 15:00</span>
+                <span>Sabados: 11:30 às 22:00</span>
+                <span>Domingos e feriados: 11:30 às 15:00</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="w-[692px]">
-          <Search placeholder="Buscar cardapio" />
-        </div>
-      </header>
-      <main className="flex flex-col items-center">
-        <div className="flex max-w-4xl flex-wrap m-auto items-center gap-6">
-          <CardCardapio />
-          <CardCardapio />
-          <CardCardapio />
-        </div>
-      </main>
+        </header>
+        <main className="flex justify-between gap-4 py-3">
+          <div className="flex flex-col max-w-[802px]">
+            <div className="w-full mb-4">
+              <Search placeholder="Buscar cardapio" />
+            </div>
+            <div>
+              <div className="px-2 py-3 font-mont font-semibold">
+                <div className="flex justify-between items-center px-3 pb-1 border-b border-black cursor-pointer">
+                  <span>Almoço</span>
+                  <span>
+                    <img src={chevronDown} alt="" />
+                  </span>
+                </div>
+              </div>
+              <div className="flex flex-wrap gap-6 font-mont py-5">
+                <CardCardapio />
+                <CardCardapio />
+                <CardCardapio />
+                <CardCardapio />
+              </div>
+              <div className="px-2 py-3 font-mont font-semibold">
+                <div className="flex justify-between items-center px-3 pb-1 border-b border-black cursor-pointer">
+                  <span>Bebidas</span>
+                  <span>
+                    <img src={chevronDown} alt="" />
+                  </span>
+                </div>
+              </div>
+              <div className="px-2 py-3 font-mont font-semibold">
+                <div className="flex justify-between items-center px-3 pb-1 border-b border-black cursor-pointer">
+                  <span>Sobremesa</span>
+                  <span>
+                    <img src={chevronDown} alt="" />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-zinc-300 w-[282px] h-[765px] hidden lg:block"></div>
+        </main>
+      </div>
     </div>
   )
 }
