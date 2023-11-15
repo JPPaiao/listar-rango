2621d58392f27012ef5d6153b6e709262a8bfc8a
jplp <jp.paiao03@gmail.com>
2023-11-15 19:55:54 -0300

    feat: Adicionado o loader da pagina de restaurante buscando pelo id

--- src/routers/Restaurant.tsx
+++ src/routers/Restaurant.tsx
@@ -2,8 +2,32 @@ import restaurantLogo from "../assets/vegan-restaurant-logo-design_1438-10@2x.pn
 import chevronDown from "../assets/chevron-down.svg"
 import { CardCardapio } from "../components/CardCardapio"
 import { Search } from "../components/Search"
+import { useLoaderData } from "react-router-dom"
 
+interface Restaurants {
+  id: number,
+  name: string,
+  address: string,
+  image: string,
+  hours: [
+    {
+      from: string,
+      to: string,
+      days: number[]
+    }
+  ]
+}
+
+const RestaurantLoader = async ({ params }) => {
+  const data = await fetch("http://localhost:3000/restaurants").then(d => d.json())
+  const dataLoader: Restaurants[] = data
+  const restaurant = dataLoader.find(r => r.id == params.id)
+
+  return restaurant
+}
 const Restaurant: React.FC = () => {
+  const restaurant: Restaurants = useLoaderData() as Restaurants
+
   return (
     <div>
       <div className="w-full bg-cyan-600 h-14 shadow-box">
@@ -15,12 +39,12 @@ const Restaurant: React.FC = () => {
               className="w-36"
             />
             <div className="max-w-md text-zinc-700 font-mont">
-              <h1 className="text-3xl font-medium">Nome do restaurante</h1>
+              <h1 className="text-3xl font-medium">{restaurant.name}</h1>
               <p className="text-sm py-2">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit dignissimos excepturi aperiam laudantium, voluptates tempora sunt nam omnis delectus deleniti
               </p>
               <div className="text-xs flex flex-col">
-                <span>Segunda à Sexta: 11:30 às 15:00</span>
+                <span>Segunda à Sexta: {restaurant.hours[0].from} às {restaurant.hours[0].to}</span>
                 <span>Sabados: 11:30 às 22:00</span>
                 <span>Domingos e feriados: 11:30 às 15:00</span>
               </div>
@@ -72,4 +96,4 @@ const Restaurant: React.FC = () => {
   )
 }
 
-export { Restaurant }
+export { Restaurant, RestaurantLoader }
