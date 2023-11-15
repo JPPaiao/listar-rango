4af3a5b2126cac36ae519cffb6ae43714434d898
jplp <jp.paiao03@gmail.com>
2023-11-15 17:03:03 -0300

    feat: Adicinado a lista de restaurants dinamicamente, consumindo a API

--- src/components/CardListRestaurant.tsx
+++ src/components/CardListRestaurant.tsx
@@ -1,19 +1,50 @@
+import { useState } from "react"
 import restaurantLogo from "../assets/vegan-restaurant-logo-design_1438-10.png"
 
-const CardListRestaurant: React.FC = () => {
+interface Restaurant {
+  id: number,
+  name: string,
+  address: string,
+  image: string,
+  hours: [
+    {
+      from: string,
+      to: string,
+      days: number[]
+    }
+  ]
+}
+
+interface Props {
+  restaurant: Restaurant
+}
+
+const CardListRestaurant: React.FC<Props> = ({ restaurant }) => {
+  const week = new Date().getDay()
+  const open = restaurant.hours[0].days.includes(week)
+
   return (
     <div>
-      <div className="font-sans flex gap-6 justify-between items-center shadow-box rounded px-3 py-5 w-full">
-        <div className="w-[106]">
-          <img src={restaurantLogo} alt="Restaurant logo" />
+      <div className="font-sans flex gap-6 justify-between items-center shadow-box rounded px-3  w-full">
+        <div className="max-w-[106px]">
+          <img src={restaurant.image} alt="Restaurant logo" />
         </div>
-        <div className="text-zinc-600 w-full font-mont">
-          <h2 className="font-semibold text-base">Nome do restaurante</h2>
-          <h3 className="text-xs">Endereço do restaurante</h3>
+        <div className="text-zinc-600 font-mont">
+          <h2 className="font-semibold text-base">{restaurant.name}</h2>
+          <h3 className="text-xs max-w-[200px]">{restaurant.address}</h3>
         </div>
       </div>
-      <div className="relative w-10 bg-indigo-900 bottom-28 left-72 p-2 text-[.50rem] text-center flex justify-center items-center rounded-full text-white font-mont">
-        Aberto agora
+      <div className="relative w-10 bg-indigo-900 bottom-28 left-80 p-2 text-[.50rem] text-center flex justify-center items-center rounded-full text-white font-mont">
+        {
+          (open) ? (
+            <>
+              Aberto
+            </>
+          ) : (
+            <>
+            Fechado</>
+          )
+        }
       </div>
     </div>
   )
--- src/routers/ListRestaurants.tsx
+++ src/routers/ListRestaurants.tsx
@@ -1,7 +1,32 @@
 import { Search } from "../components/Search"
 import { CardListRestaurant } from "../components/CardListRestaurant"
+import { useEffect, useState } from "react"
+
+interface Restaurants {
+  id: number,
+  name: string,
+  address: string,
+  image: string,
+  hours: [
+    {
+      from: string,
+      to: string,
+      days: number[]
+    }
+  ]
+}
 
 const ListRestaurants: React.FC = () => {
+  const [restaurants, setRestaurants] = useState<Restaurants[]>([])
+
+  useEffect(() => {
+    const list = async () => {
+      const data = await fetch("http://localhost:3000/restaurants").then(d => d.json())
+      setRestaurants(data)
+    }
+    list()
+  }, [])
+
   return (
     <div className="h-screen font-mont">
       <header className="flex gap-5 flex-col items-center">
@@ -12,13 +37,18 @@ const ListRestaurants: React.FC = () => {
         </h1>
       </header>
       <main className="px-2">
-        <div className="w-[840px] py-6 m-auto">
+        <div className="max-w-[840px] py-6 m-auto">
           <Search placeholder={"Buscar estabelecimento"} />
         </div>
-        <div className="flex justify-evenly items-center flex-wrap gap-2 py-3">
-          <CardListRestaurant />
-          <CardListRestaurant />
-          <CardListRestaurant />
+        <div className="flex justify-evenly items-center flex-wrap gap-2 py-3 px-4">
+          {
+            restaurants.map((value: Restaurants) => {
+
+              return (
+                <CardListRestaurant restaurant={value}/>
+              )
+            })
+          }
         </div>
       </main>
     </div>
