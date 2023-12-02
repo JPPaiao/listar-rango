665d5e74856fae0019fdadbe3dfbbaad7d96f571
jplp <jp.paiao03@gmail.com>
2023-12-01 22:49:50 -0300

    refactor: Preço total do modal adicionado dinamicamente

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,24 @@
 import { useLocation } from "react-router-dom"
 import pratoRestaurant from "../assets/prato-de-restaurante-vegetariano.png"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { PropsModal } from "../interfaces"
 
 const Modal: React.FC<PropsModal> = ({ menu }) => {
-  const [add, setAdd] = useState<number>(1)
+  const price = menu.sales[0].price
   const location = useLocation()
   const open = location.hash ? "block" : "hidden"
+  const [add, setAdd] = useState<number>(1)
+  const [total, setTotal] = useState<number>(price * add)
+
+  const handleSetValue = (description: string) => {
+    if (description === 'increment') setAdd(add + 1) 
+    if (description === 'decrement') setAdd(add - 1)
+  }
+
+  useEffect(() => {
+    if (add <= 0) setTotal(0)
+    if (add >= 1) setTotal(price * add)
+  }, [add, price])
 
   return (
     <div id={`${menu.name}Modal`} className={`${open} w-full fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-50 z-50 font-mont shadow-box px-5`}>
@@ -20,24 +32,24 @@ const Modal: React.FC<PropsModal> = ({ menu }) => {
             <h1 className="text-2xl mb-2">{menu.name}</h1>
             <div className="flex sm:justify-between flex-col sm:flex-row items-start gap-2 sm:gap-4 text-base sm:items-end leading-tight">
               <span className="max-w-sm">{menu.sales[0].description}</span>
-              <span className="text-cyan-600 text-3xl">R$ {menu.sales[0].price},00</span>
+              <span className="text-cyan-600 text-3xl">R$ {price},00</span>
             </div>
           </div>
           <footer className="border-t border-solid border-black border-opacity-30 w-full flex justify-between px-3 py-4">
-            <div className="px-4 py-1 border border-solid border-black border-opacity-30 rounded flex gap-2 text-cyan-600 font-bold max-w-xs z-50 cursor-pointer">
-              <div className="p-1" onClick={() => setAdd(add - 1)}>
+            <div className="px-2 py-1 border border-solid border-black border-opacity-30 rounded flex gap-4 text-cyan-600 font-bold max-w-xs z-50 cursor-pointer items-center justify-between">
+              <div className="px-[2px]" onClick={() => handleSetValue('decrement')}>
                 -
               </div>
-              <div className="p-1 relative top-0 bottom-0 left-0 right-0 z-50">
+              <div className=" relative top-0 bottom-0 left-0 right-0 z-50">
                 {add}
               </div>
-              <div className="p-1" onClick={() => setAdd(add + 1)}>
+              <div className="px-[2px]" onClick={() => handleSetValue('increment')}>
                 +
               </div>
             </div>
             <button className="bg-cyan-600 px-2 py-1 text-lg text-white flex items-center justify-between gap-10 rounded-sm cursor-pointer hover:bg-cyan-800 transition">
               <span>Adicionar</span>
-              <span>R$ {menu.sales[0].price},00</span>            
+              <span>R$ {total},00</span>            
             </button>
           </footer>
         </div>
